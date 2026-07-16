2ad1adcce7bd89301746c9a7da7b16e696328fdd
Malaz2p6 <malazalbadawi7@gmail.com>
2026-07-16 19:49:36 +0300

    Add recipe list and detail pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
